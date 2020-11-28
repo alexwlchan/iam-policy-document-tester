790a4f85ad2daeba77c9d4b38f5528d62d7994d5
Alex Chan <alex@alexwlchan.net>
2020-11-28 21:39:33 +0000

    Add a big docstring to the top of the file

--- iam_tester.py
+++ iam_tester.py
@@ -1,3 +1,35 @@
+"""
+Create some temporary IAM credentials that use a particular policy document,
+then create a client that uses that role.  This is useful for rapid testing and
+experiments with IAM policy documents.
+
+    from iam_tester import (
+        create_aws_client_from_credentials,
+        temporary_iam_credentials
+    )
+
+    admin_role_arn = "arn:aws:iam::760097843905:role/platform-admin"
+
+    policy_document = {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Effect": "Allow",
+                "Action": "s3:List*",
+                "Resource": "*"
+            },
+        ]
+    }
+
+    with temporary_iam_credentials(
+        admin_role=admin_role_arn, policy_document=policy_document
+    ) as credentials:
+        s3_client = create_aws_client_from_credentials("s3", credentials=credentials)
+
+        # do stuff with S3
+
+"""
+
 import contextlib
 import datetime as dt
 import json
